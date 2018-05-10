272e2443d5bfbc832239e7694a9dd899172313e9
swapsCAPS <verstegen.daan@gmail.com>
2018-05-10 15:00:36 +0200

    name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -133,12 +133,12 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_MKS_GEN_13
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
 // Displayed in the LCD "Ready" message
-#define CUSTOM_MACHINE_NAME "FT-5"
+#define CUSTOM_MACHINE_NAME "Le printer"
 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
